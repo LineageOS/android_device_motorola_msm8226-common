904f11fad27edd11e661f3f22a892dbc78d68c13
Gabriele M <moto.falcon.git@gmail.com>
2017-02-13 21:51:40 +0100

    releasetools: Remove unused import and variable
    Change-Id: Idc65b6e6db1d852c865ab977d749e24ffc8aed40

--- releasetools.py
+++ releasetools.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,9 +14,6 @@
 # limitations under the License.
 
 import re
-import os
-
-TARGET_DIR = os.getenv('OUT')
 
 def FullOTA_Assertions(info):
   AddBootloaderAssertion(info, info.input_zip)
