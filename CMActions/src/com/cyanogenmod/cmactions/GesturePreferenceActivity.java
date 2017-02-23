0efe2cbb5588eace327ca08fe3e9f17284033384
Gabriele M <moto.falcon.git@gmail.com>
2017-02-23 18:31:38 +0100

    CMActions: Move to Display settings
    Instead of adding a tile to the preferences, replace the
    Ambient Display entry with CMActions.
    
    Change-Id: I00205c9d6a18bb85372c863ac33505b8f61ebc1f

--- CMActions/src/com/cyanogenmod/cmactions/GesturePreferenceActivity.java
+++ CMActions/src/com/cyanogenmod/cmactions/GesturePreferenceActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 The CyanogenMod Project
+ * Copyright (c) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +26,7 @@ public class GesturePreferenceActivity extends SettingsDrawerActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        showMenuIcon();
         getFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, new GesturePreferenceFragment()).commit();
     }
